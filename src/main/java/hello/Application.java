aed1ee75942c6b046180abbb7efed0b6927e5987
Ткаченко Александр <tkachenko.ay@reliab.tech>
2017-11-02 18:28:51 +0300

    test13

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -17,5 +17,6 @@ public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        System.out.println("test");
     }
 }
